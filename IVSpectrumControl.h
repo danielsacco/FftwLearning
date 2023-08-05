eacade6de86ee35bd05b382e82d70ed913580956
Daniel Sacco <daniel.sacco@gmail.com>
2023-08-05 07:14:39 -0300

    Start working on a future IVSpectrumControl

---
+++ IVSpectrumControl.h
@@ -0,0 +1,12 @@
+//
+//  IVSpectrumControl.h
+//  VST3
+//
+//  Created by Daniel Sacco on 04/08/2023.
+//
+
+#ifndef IVSpectrumControl_h
+#define IVSpectrumControl_h
+
+
+#endif /* IVSpectrumControl_h */
